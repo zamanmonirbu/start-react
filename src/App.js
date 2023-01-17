8f8e1858ab8056c3d49efa404e12b71a8d05ede2
MD. MONIRUZZAMAN <53488876+zamanmonirbu@users.noreply.github.com>
2023-01-17 11:14:01 +0600

    basic template of react

--- src/App.js
+++ src/App.js
@@ -3,20 +3,43 @@ import './App.css';
 
 function App() {
 
-// {/* <style>
-//   .name{
-//     color:red;
-//   } */}
-// </style>
-
+  const nayok=['rajjak','manna','Jasim'];
+  const profes=['nayok'];
 
   return (
     <div className="App">
       <header className="App-header">
-       <p className='name' style={{color:'red'}}>Monir</p>
+       <p>Monir</p>
+       <Person name="Monir" profession="Student"></Person>
+       <Person name={nayok[0]} profession={profes[0]}></Person>
+       <Person></Person>
       </header>
     </div>
   );
 }
+
+
+
+
+
+
+function Person(data){
+  const rong={
+    border:'1px solid blue',
+    margin:'5px',
+    padding:'10px'
+  }
+  return(
+    <div style={rong}>
+      <h1>Name: {data.name}</h1>
+      <p>Profession: {data.profession}</p>
+    </div>
+  )
+}
+
+
+
+
+
 
 export default App;
